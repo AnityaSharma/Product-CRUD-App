b8516492c119248b43a8b738c26cfb627252380f
unknown <anityas4@gmail.com>
2023-07-13 15:20:35 +0530

    modified Home page, Added delete operation

--- src/main/java/productcrudapp/control/MainController.java
+++ src/main/java/productcrudapp/control/MainController.java
@@ -1,11 +1,14 @@
 package productcrudapp.control;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.view.RedirectView;
@@ -20,12 +23,16 @@ public class MainController {
 	private ProductDao productDao;
 	
 	@RequestMapping("/first")
-	public String home() {
+	public String home(Model m) {
+		
+		List<Product> products=productDao.getAllProducts();
+		m.addAttribute("product",products);
+		
 		System.out.println("Running Controller");
 		return "indexx";
 	}
 	
-	@RequestMapping(value="add-product")
+	@RequestMapping(value="/add-product")
 	public String addProduct(Model m) {
 		m.addAttribute("title","Add Product");
 		return "addProductForm";
@@ -40,6 +47,15 @@ public class MainController {
 		
 		redirectView.setUrl(request.getContextPath()+"/");
 		
+		return redirectView;
+	}
+	
+	//To delete a product
+	@RequestMapping("/delete/{productId}")
+	public RedirectView deleteProduct(@PathVariable("productId") int productId,HttpServletRequest request) {
+		productDao.deleteProduct(productId);
+		RedirectView redirectView= new RedirectView();
+		redirectView.setUrl(request.getContextPath()+"/");
 		return redirectView;
 	}
 	
